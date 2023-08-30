d8e7b2d232d8723900238990b396935323474f8d
Joey Lim <mail@joeylim.xyz>
2023-08-30 17:11:24 -0400

    Updated responsive sizing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Model } from './Model';
 import { Canvas, useThree } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 
 function App() {
+  const [modelScale, setModelScale] = useState([4.8, 4.8, 4.8]);
+  const [cameraPosition, setCameraPosition] = useState([0.2, 2, 5.2]);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 768) {
+        setModelScale([2, 2, 2]);
+        setCameraPosition([0.35, 2.85, 3.8]);
+      } else {
+        setModelScale([4.8, 4.8, 4.8]);
+        setCameraPosition([0.2, 2, 5.2]);
+      }
+    }
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className="canvas-container">
       <Canvas>
-        <SetupCamera />
-        <ambientLight intensity={0.5} />
+        <SetupCamera position={cameraPosition} />
+        <ambientLight intensity={0.8} />
         <directionalLight position={[0, 10, 5]} intensity={1} />
-        <Model scale={[7,7,7]} />
+        <Model scale={modelScale} />
         <OrbitControls />
       </Canvas>
       <p className="smoothed">
@@ -21,13 +40,13 @@ function App() {
   );
 }
 
-function SetupCamera() {
+function SetupCamera({ position }) {
   const { camera, set } = useThree();
 
   useEffect(() => {
-    camera.position.set(0.2, 2.5, 5.2);
+    camera.position.set(...position);
     set({ camera });
-  }, [camera, set]);
+  }, [camera, set, position]);
 
   return null;
 }
--- src/Model.js
+++ src/Model.js
@@ -1,43 +1,36 @@
-import React, { useRef } from "react";
+import React from "react";
 import { useGLTF } from "@react-three/drei";
 
 useGLTF.preload('/model.gltf');
 
-export function Model(props) {
+export function Model({ scale = [1, 1, 1] }) {
   const { nodes, materials } = useGLTF("/lifeworld.glb");
   return (
-    <group {...props} dispose={null}>
-      <group position={[0, -0.5, 0]} rotation={[0, 0, 0]} scale={0.75}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Mesh_0.geometry}
-          material={materials.Material_0}
-          position={[-1.3, 0.2, -1.98]}
-          rotation={[0, -1.05, 0]}
-          scale={0.95}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Mesh_0001.geometry}
-          material={materials["Material_0.001"]}
-          position={[0.9, 0.34, -1.69]}
-          rotation={[0, 0.84, 0]}
-          scale={1.06}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Mesh_0002.geometry}
-          material={materials["Material_0.002"]}
-          position={[-0.18, 0.28, -1.98]}
-          rotation={[-Math.PI, 1.54, -Math.PI]}
-          scale={0.95}
-        />
+    <group scale={scale} dispose={null}>
+      <group position={[0, -0.5, 0]} rotation={[0, 0, 0]}>
+        <group position={[-0.15, 0.2, -2.1]} rotation={[-Math.PI, 1.543, -Math.PI]}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Mesh_0002_1.geometry}
+            material={materials["Material_0.002"]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Mesh_0002_2.geometry}
+            material={materials.Material_0}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Mesh_0002_3.geometry}
+            material={materials["Material_0.001"]}
+          />
+        </group>
       </group>
     </group>
   );
 }
 
-useGLTF.preload("/lifeworld.glb");
+useGLTF.preload("/lifeworld2.glb");
