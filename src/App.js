ce9b6c303777bab0ccd9b840fcb947962434217d
Joey Lim <mail@joeylim.xyz>
2023-09-01 16:13:10 -0400

    show mail address during loading pt.2

--- src/App.js
+++ src/App.js
@@ -2,25 +2,50 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Model } from './Model';
 import { Canvas, useThree } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import Loading from './Loading'; 
-import { useProgress } from '@react-three/drei'; 
+import { OrbitControls, useProgress } from '@react-three/drei';
+import Loading from './Loading';
 
 function App() {
-  const [modelScale, setModelScale] = useState([5, 5, 5]);
   const [cameraPosition, setCameraPosition] = useState([0.2, 1.94, 5.2]);
-  const [modelPosition, setModelPosition] = useState([0, 0, 0]);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 768) {
+        setCameraPosition([-3, 3.7, 4]);
+      } else {
+        setCameraPosition([0.35, 2.4, 5.2]);
+      }
+    }
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <div className="canvas-container">
+      <Canvas>
+        <CanvasContent cameraPosition={cameraPosition} />
+      </Canvas>
+      <p className="smoothed">
+        <a href="mailto:everything@lifeworld.co">everything@lifeworld.co</a>
+      </p>
+    </div>
+  );
+}
+
+function CanvasContent({ cameraPosition }) {
   const { active, progress, total } = useProgress();
+  const [modelScale, setModelScale] = useState([5, 5, 5]);
+  const [modelPosition, setModelPosition] = useState([0, 0, 0]);
 
   useEffect(() => {
     function handleResize() {
       if (window.innerWidth <= 768) {
         setModelScale([2.7, 2.7, 2.7]);
-        setCameraPosition([-3, 3.7, 4]);
         setModelPosition([3.8, -1.5, 0.3]);
       } else {
         setModelScale([4.4, 4.4, 4.4]);
-        setCameraPosition([0.35, 2.4, 5.2]);
         setModelPosition([-0.2, -0.14, 0.8]);
       }
     }
@@ -30,23 +55,14 @@ function App() {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  if (active && progress !== total) {
-    return <Loading />;
-  }
-
   return (
-    <div className="canvas-container">
-      <Canvas>
-        <SetupCamera position={cameraPosition} />
-        <ambientLight intensity={1.5} />
-        <directionalLight position={[5, 10, 5]} intensity={1} />
-        {active && progress !== total ? <Loading /> : <Model scale={modelScale} position={modelPosition} />}
-        <OrbitControls />
-      </Canvas>
-      <p className="smoothed">
-        <a href="mailto:everything@lifeworld.co">everything@lifeworld.co</a>
-      </p>
-    </div>
+    <>
+      <SetupCamera position={cameraPosition} />
+      <ambientLight intensity={1.5} />
+      <directionalLight position={[5, 10, 5]} intensity={1} />
+      {active && progress !== total ? <Loading /> : <Model scale={modelScale} position={modelPosition} />}
+      <OrbitControls />
+    </>
   );
 }
 
