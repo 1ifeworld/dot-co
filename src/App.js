db106acdf502f81cb839050db0b8ba39a2467725
Joey Lim <mail@joeylim.xyz>
2023-09-01 17:03:00 -0400

    trying again re: loading animation + email link

--- src/App.js
+++ src/App.js
@@ -2,50 +2,25 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Model } from './Model';
 import { Canvas, useThree } from '@react-three/fiber';
-import { OrbitControls, useProgress } from '@react-three/drei';
-import Loading from './Loading';
+import { OrbitControls } from '@react-three/drei';
+import Loading from './Loading'; 
+import { useProgress } from '@react-three/drei'; 
 
 function App() {
-  const [cameraPosition, setCameraPosition] = useState([0.2, 1.94, 5.2]);
-
-  useEffect(() => {
-    function handleResize() {
-      if (window.innerWidth <= 768) {
-        setCameraPosition([-3, 3.7, 4]);
-      } else {
-        setCameraPosition([0.35, 2.4, 5.2]);
-      }
-    }
-
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  return (
-    <div className="canvas-container">
-      <Canvas>
-        <CanvasContent cameraPosition={cameraPosition} />
-      </Canvas>
-      <p className="smoothed">
-        <a href="mailto:everything@lifeworld.co">everything@lifeworld.co</a>
-      </p>
-    </div>
-  );
-}
-
-function CanvasContent({ cameraPosition }) {
-  const { active, progress, total } = useProgress();
   const [modelScale, setModelScale] = useState([5, 5, 5]);
+  const [cameraPosition, setCameraPosition] = useState([0.2, 1.94, 5.2]);
   const [modelPosition, setModelPosition] = useState([0, 0, 0]);
+  const { active, progress, total } = useProgress();
 
   useEffect(() => {
     function handleResize() {
       if (window.innerWidth <= 768) {
         setModelScale([2.7, 2.7, 2.7]);
+        setCameraPosition([-3, 3.7, 4]);
         setModelPosition([3.8, -1.5, 0.3]);
       } else {
         setModelScale([4.4, 4.4, 4.4]);
+        setCameraPosition([0.35, 2.4, 5.2]);
         setModelPosition([-0.2, -0.14, 0.8]);
       }
     }
@@ -55,14 +30,27 @@ function CanvasContent({ cameraPosition }) {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
+  if (active && progress !== total) {
+    return <Loading />;
+  }
+
   return (
-    <>
-      <SetupCamera position={cameraPosition} />
-      <ambientLight intensity={1.5} />
-      <directionalLight position={[5, 10, 5]} intensity={1} />
-      {active && progress !== total ? <Loading /> : <Model scale={modelScale} position={modelPosition} />}
-      <OrbitControls />
-    </>
+    <div className="canvas-container">
+      {active && progress !== total ? (
+        <Loading />
+      ) : (
+        <Canvas>
+          <SetupCamera position={cameraPosition} />
+          <ambientLight intensity={1.5} />
+          <directionalLight position={[5, 10, 5]} intensity={1} />
+          <Model scale={modelScale} position={modelPosition} />
+          <OrbitControls />
+        </Canvas>
+      )}
+      <p className="smoothed">
+        <a href="mailto:everything@lifeworld.co">everything@lifeworld.co</a>
+      </p>
+    </div>
   );
 }
 
--- src/Loading.js
+++ src/Loading.js
@@ -1,64 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { Model } from './Model';
-import { Canvas, useThree } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import Loading from './Loading'; 
-import { useProgress } from '@react-three/drei'; 
+import React, { useState, useEffect } from 'react';
+import './Loading.css';
 
-function App() {
-  const [modelScale, setModelScale] = useState([5, 5, 5]);
-  const [cameraPosition, setCameraPosition] = useState([0.2, 1.94, 5.2]);
-  const [modelPosition, setModelPosition] = useState([0, 0, 0]);
-  const { active, progress, total } = useProgress();
+function Loading() {
+  const [dotCount, setDotCount] = useState(1);
 
   useEffect(() => {
-    function handleResize() {
-      if (window.innerWidth <= 768) {
-        setModelScale([2.7, 2.7, 2.7]);
-        setCameraPosition([-3, 3.7, 4]);
-        setModelPosition([3.8, -1.5, 0.3]);
-      } else {
-        setModelScale([4.4, 4.4, 4.4]);
-        setCameraPosition([0.35, 2.4, 5.2]);
-        setModelPosition([-0.2, -0.14, 0.8]);
-      }
-    }
-
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    const interval = setInterval(() => {
+      setDotCount((prevCount) => (prevCount === 3 ? 1 : prevCount + 1));
+    }, 500);
+    return () => clearInterval(interval);
   }, []);
 
-  if (active && progress !== total) {
-    return <Loading />;
-  }
-
   return (
-    <div className="canvas-container">
-      <Canvas>
-        <SetupCamera position={cameraPosition} />
-        <ambientLight intensity={1.5} />
-        <directionalLight position={[5, 10, 5]} intensity={1} />
-        {active && progress !== total ? <Loading /> : <Model scale={modelScale} position={modelPosition} />}
-        <OrbitControls />
-      </Canvas>
-      <p className="smoothed">
-        <a href="mailto:everything@lifeworld.co">everything@lifeworld.co</a>
+    <div className="loading-container">
+      <p className="loading-text">
+        loading
+        <span style={{ opacity: dotCount >= 1 ? 1 : 0 }}>.</span>
+        <span style={{ opacity: dotCount >= 2 ? 1 : 0 }}>.</span>
+        <span style={{ opacity: dotCount >= 3 ? 1 : 0 }}>.</span>
       </p>
     </div>
   );
 }
 
-function SetupCamera({ position }) {
-  const { camera, set } = useThree();
-
-  useEffect(() => {
-    camera.position.set(...position);
-    set({ camera });
-  }, [camera, set, position]);
-
-  return null;
-}
-
-export default App;
+export default Loading;
